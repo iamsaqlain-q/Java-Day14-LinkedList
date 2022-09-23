f5ebf2695ed3a98b4efaddb58667223400d582d1
Saqlain <iamkhatik786@gmail.com>
2022-09-23 21:41:24 +0530

    add 30

--- LinkedLists.java
+++ LinkedLists.java
@@ -14,8 +14,38 @@ public class LinkedLists<T> {
 	      temp.next = newNode;
 	    }
 	  }
+	  
+	  public void addFirst(T item) {
+		    Node<T> newNode = new Node<>(item);
 
-	}
+		    if (head == null) head = newNode;
+		    else {
+		      newNode.next = head;
+		      head = newNode;
+		    }
+		  }
+
+		  public void display() {
+		    if (isEmpty()) {
+		      System.out.println("list empty! nothing to display");
+		      return;
+		    }
+
+		    Node<T> temp = head;
+		    while (temp.next != null) {
+		      System.out.print(temp.data + " -> ");
+		      temp = temp.next;
+		    }
+		    System.out.println(temp.data);
+		  }
+
+		  private boolean isEmpty() {
+		    if (head == null) 
+		    	return true;
+		    return false;
+		  }
+
+		}
 
 	class Node<T> {
 
--- List.java
+++ List.java
@@ -1,12 +1,13 @@
 package com.bl.linkedlist;
 
 public class List {
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 
-	    LinkedLists<Integer> linkedL = new LinkedLists<>();
+		    LinkedLists<Integer> linkedL = new LinkedLists<>();
 
-	    linkedL.addLast(56);
-	    linkedL.addLast(30);
-	    linkedL.addLast(70);
-	  }
+		    linkedL.addFirst(70);
+		    linkedL.addFirst(30);
+		    linkedL.addFirst(56);
+		    linkedL.display();
+		  }
 }
